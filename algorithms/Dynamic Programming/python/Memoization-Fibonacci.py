2ce5b62e026cf0caad9e7f7955657903d77a2d10
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-07-05 01:30:54 -0400

    explain

---
+++ algorithms/Dynamic Programming/python/Memoization-Fibonacci.py
@@ -0,0 +1,20 @@
+import time
+start_time = time.time()
+
+def fib_top_down(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    memo[n] = fib_top_down(n-1, memo) + fib_top_down(n-2, memo)
+    return memo[n]
+
+N = int(input())
+result = fib_top_down(N)
+end_time = time.time()
+execution_time = end_time - start_time
+
+print(f"The Fibonacci number at position {N} is {result}")
+print(f"Execution time: {execution_time} seconds")
+# creating the dict memo allows us to avoid redundant computations, not possible with plain recursion   
+
---
+++ algorithms/Dynamic Programming/python/Tabulation-Fibonacci.py
@@ -0,0 +1,25 @@
+import time
+start_time = time.time()
+
+N = int(input())
+dp = [0] * N
+
+# redefine first two elements in the array to be 1 and 1 as they are your base cases
+dp[0], dp[1] = 1, 1
+
+for i in range(2, N):
+    dp[i] = dp[i-1] + dp[i-2]
+
+result = dp[N-1]
+for i in range(N):
+    for j in range(i+1, N):
+        res = dp[j]
+
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"The Fibonacci number at position {N} is {result}")
+print(f"Execution time: {execution_time} seconds")
+# iteratively compute each Fibonacci number from the bottom up. This approach avoids the need for recursion and memoization.
+
+# fib(1), fib(2), fib(3) get saved as base cases in the array
+# this allows us to skip having to recompute them if we were using recursion
